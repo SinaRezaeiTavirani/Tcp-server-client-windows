5ffc4ee7d9321bd86b19a61e160bf2ee9bb66962
SinaRezaeiTavirani <sinaezar@yahoo.com>
2023-11-03 01:13:09 +0330

    Refactor server code
    improved readability and modularization

--- server.cpp
+++ server.cpp
@@ -3,90 +3,91 @@
 #include <ws2tcpip.h>
 #include <tchar.h>
 
-int main() 
-{
-	// INITIALIZE WSAStartup
+const int PORT = 5555;
 
-	WSADATA wsa_data;
-	int wsa_err;
+bool initializeWSA(WSADATA& wsa_data) {
+    WORD m_version_requested = MAKEWORD(2,2);
+    int wsa_err = WSAStartup(m_version_requested, &wsa_data);
 
-	// MAKEWORD creates a certain format which is 2.2 for starting our windows socket
-	WORD m_version_requested = MAKEWORD(2,2);
-	// wsa_err is for checking if our dll is found or not
-	wsa_err = WSAStartup(m_version_requested, &wsa_data);
+    if (wsa_err != 0) {
+        std::cout << "The Winsock dll not found!" << std::endl;
+        return false;
+    }
+    
+    std::cout << "The Winsock dll found" << std::endl;
+    std::cout << "The status: " << wsa_data.szSystemStatus << std::endl;
+    return true;
+}
 
-	if(wsa_err != 0)
-	{
-		std::cout << "The Winsock dll not found!" << std::endl;
-		return 0;		
-	}
-	else 
-	{
-		std::cout << "The Winsock dll found" << std::endl;
-		std::cout << "The status: " << wsa_data.szSystemStatus << std::endl;
-	}
+SOCKET createSocket() {
+    SOCKET server_socket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+    
+    if (server_socket == INVALID_SOCKET) {
+        std::cout << "ERROR at socket(): " << WSAGetLastError() << std::endl;
+        WSACleanup();
+    } else {
+        std::cout << "socket() is OK!" << std::endl;
+    }
+    
+    return server_socket;
+}
 
-	// CREAT SOCKET
+bool bindSocket(SOCKET server_socket) {
+    sockaddr_in service;
+    service.sin_family = AF_INET;
+    InetPton(AF_INET, _T("127.0.0.1"), &service.sin_addr.s_addr);
+    service.sin_port = htons(PORT);
 
-	SOCKET server_socket = INVALID_SOCKET;
-	int port = 5555;
+    if (bind(server_socket, (SOCKADDR*)&service, sizeof(service)) == SOCKET_ERROR) {
+        std::cout << "bind() failed " << WSAGetLastError() << std::endl;
+        closesocket(server_socket);
+        WSACleanup();
+        return false;
+    }
 
-	// This code is for tcp so we select sock_stream for tcp and if udp we pass SOCK_DGRAM
-	// af_inet means a family that contains udp and tcp 
-	// The protocol for TCP is IPPROTO_TCP
-	// returns 0 if successful
-	server_socket = socket(AF_INET,
-	 						SOCK_STREAM,
-	 						IPPROTO_TCP);
+    std::cout << "bind() is OK!" << std::endl;
+    return true;
+}
 
-	if(server_socket == INVALID_SOCKET)
-	{
-		std::cout << "ERROR at socket(): " << WSAGetLastError() << std::endl;
-		WSACleanup();
-		return 0;
-	}
-	else
-	{
-		std::cout << "socket() is OK!" << std::endl;
-	}
+bool startListening(SOCKET server_socket) {
+    if (listen(server_socket, 2) == SOCKET_ERROR) {
+        std::cout << "listen() ERROR listening on socket: " << WSAGetLastError() << std::endl;
+        return false;
+    }
 
-	// BIND SOCKET
+    std::cout << "listen() is OK" << std::endl;
+    return true;
+}
 
-	sockaddr_in service;
-	service.sin_family = AF_INET;
-	InetPton(AF_INET, _T("127.0.0.1"), &service.sin_addr.s_addr);
-	service.sin_port = htons(port);
-	if(bind(server_socket, (SOCKADDR*)&service, sizeof(service)) == SOCKET_ERROR)
-	{
-		std::cout << "bind() failed " << WSAGetLastError() << std::endl;
-		closesocket(server_socket);
-		WSACleanup();
-		return 0;
-	}
-	else 
-	{
-		std::cout << "bind() is OK!" << std::endl;
-	}
+SOCKET acceptConnection(SOCKET server_socket) {
+    SOCKET accept_socket = accept(server_socket, nullptr, nullptr);
 
-	// LISTEN 
+    if (accept_socket == INVALID_SOCKET) {
+        std::cout << "accept failed" << WSAGetLastError() << std::endl;
+        return INVALID_SOCKET;
+    }
 
-	if(listen(server_socket, 2) == SOCKET_ERROR)
-		std::cout << "listen() ERROR listening on socket: " << WSAGetLastError() << std::endl;
-	else
-		std::cout << "listen() is OK" << std::endl;
+    std::cout << "accepted connection" << std::endl;
+    return accept_socket;
+}
 
-	SOCKET accept_socket = INVALID_SOCKET;
-	accept_socket = accept(server_socket, nullptr, nullptr);
-	if(accept_socket == INVALID_SOCKET)
-	{
-		std::cout << "accept failed" << WSAGetLastError() << std::endl;
-		return -1;	
-	}
+int main() {
+    WSADATA wsa_data;
 
-	std::cout << "accepted connection" << std::endl;
+    if (!initializeWSA(wsa_data)) return 0;
 
-	closesocket(server_socket);
-	WSACleanup();
+    SOCKET server_socket = createSocket();
+    if (server_socket == INVALID_SOCKET) return 0;
 
-	return 0;
+    if (!bindSocket(server_socket)) return 0;
+
+    if (!startListening(server_socket)) return 0;
+
+    SOCKET accept_socket = acceptConnection(server_socket);
+    if (accept_socket == INVALID_SOCKET) return -1;
+
+    closesocket(server_socket);
+    WSACleanup();
+
+    return 0;
 }
